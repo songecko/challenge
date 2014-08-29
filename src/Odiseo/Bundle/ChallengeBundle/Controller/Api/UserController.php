b5e05044b0651f3bec6a04269d43877217251d16
Diego <songecko@gmail.com>
2014-08-29 01:55:12 -0300

    search method

--- src/Odiseo/Bundle/ChallengeBundle/Controller/Api/UserController.php
+++ src/Odiseo/Bundle/ChallengeBundle/Controller/Api/UserController.php
@@ -6,12 +6,15 @@ use FOS\RestBundle\Controller\FOSRestController;
 use Odiseo\Bundle\ChallengeBundle\Entity\User;
 use FOS\RestBundle\View\View;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use FOS\RestBundle\Controller\Annotations\QueryParam;
+use FOS\RestBundle\Controller\Annotations\Route;
 
 class UserController extends FOSRestController
 {
 	/**
 	 * Get the user information by id.
 	 *
+	 * @Route(requirements={"id"="\d+"})
 	 * @ApiDoc(
 	 *  resource=true,
 	 *  description="Get the user information."
@@ -21,6 +24,28 @@ class UserController extends FOSRestController
 	{
 		$data = $this->get('odiseo_challenge.repository.user')->find($id);
 		
+		return $this->handleView($this->view($data, 200));
+	}
+	
+	/**
+	 * Search users.
+	 * 
+	 * @QueryParam(name="name", description="Name of user")
+	 * @ApiDoc(
+	 *  resource=true,
+	 *  description="Search users."
+	 * )
+	 */
+	public function searchAction()
+	{
+		$name = $this->getRequest()->get('name');
+		$data = $this->get('odiseo_challenge.repository.user')->createQueryBuilder('u')
+			->where('u.name LIKE :name')
+			->setParameter('name', '%'.$name.'%')
+			->getQuery()
+			->getResult()
+		;
+	
 		return $this->handleView($this->view($data, 200));
 	}
 }
